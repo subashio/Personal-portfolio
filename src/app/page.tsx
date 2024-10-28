d50057b34cf26e182c0610f562bf72f92c023ec9
M Subash <105859085+Git-Subash@users.noreply.github.com>
2024-10-28 17:57:57 +0530

    scroll-to-top

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,12 @@ import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 
 import Services from "@/components/Services";
+import ScrollProgressBar from "@/components/ui/scroll-progress-bar";
 
 export default function Home() {
   return (
     <main className="relative mx-auto flex flex-col items-center justify-center overflow-hidden">
+      <ScrollProgressBar type="circle" color="#3e8bf0" showPercentage={true} />
       <Hero />
       <About />
       <Services />
