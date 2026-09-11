5691df34979b6047bff201e87e069eb727c04515
M Subash <subashotherp11@gmail.com>
2026-09-12 01:04:49 +0530

    modified-gtmId

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -73,7 +73,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full !scroll-smooth">
-      <GoogleTagManager gtmId="G-2XY8VGZYZW" />
+      <GoogleTagManager gtmId="G-TGCRK3LLNN" />
       <GoogleAnalytics gaId="G-2XY8VGZYZW" />
       <body
         className={cn(
